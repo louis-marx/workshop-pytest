c89bc3fb7a712b74d3ec8c411ea8e7e60feec713
Louis Marx <louis.marx@mailo.com>
2024-10-15 11:34:47 +0200

    add calculate module

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+# python -m doctest -v main.py
+
+
 def to_absolute(number):
-    """
-    >>> to_absolute(3)
-    3
-    >>> to_absolute(-3)
-    0
-    """
     if number <= 0:
         return -number
     return number
